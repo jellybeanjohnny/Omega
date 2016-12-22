d4e94dacbbf2eb0b8c09ce9dc51ed2f04b8d15e8
jellybeanjohnny <mamerige@gmail.com>
2016-12-21 16:13:14 -0800

    Initial Commit

---
+++ OmegaTests/OmegaTests.swift
@@ -0,0 +1,36 @@
+//
+//  OmegaTests.swift
+//  OmegaTests
+//
+//  Created by Matt Amerige on 12/21/16.
+//  Copyright © 2016 Matt Amerige. All rights reserved.
+//
+
+import XCTest
+@testable import Omega
+
+class OmegaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
